fd25bc996e2b8f3b5e36b8111a9c450ad235c098
hunter5299 <75543216+hunter5299@users.noreply.github.com>
2021-04-21 23:44:04 +0800

    Add files via upload

--- 2021-4-17-栈与队列的增删查改/stack.c
+++ 2021-4-17-栈与队列的增删查改/stack.c
@@ -9,55 +9,48 @@
 //	int _capacity;  // 容量 
 //}Stack;
 // 初始化栈 
+//先实现栈的数据结构
 void StackInit(Stack* ps){
-	assert(ps);
 	ps->_a = (STDataType*)malloc(sizeof(STDataType)* 100);
 	ps->_top = 0;
 	ps->_capacity = 100;
 }
 //检查容量
 void STCapCheck(Stack* ps){
-	assert(ps);
 	if (ps->_capacity <= ps->_top){
-		int newCap = sizeof(STDataType)* (ps->_capacity + 100);
-		ps->_a = (STDataType*)realloc(ps->_a,newCap );
-		assert(ps->_a);
+		int newCap = sizeof(STDataType)* (ps->_capacity + 10);
+		ps->_a = (STDataType*)realloc(ps->_a, newCap);
 		ps->_capacity = newCap;
 	}
 }// 入栈 
-void StackPush(Stack* ps, STDataType data){
-	assert(ps);
+int StackPush(Stack* ps, STDataType data){
 	STCapCheck(ps);
 	ps->_a[ps->_top] = data;
 	ps->_top++;
+	return data;
 }
 // 出栈 
-void StackPop(Stack* ps){
-	assert(ps);
-	if (!StackEmpty){
-		ps->_top --;
+int StackPop(Stack* ps){
+	if (StackEmpty(ps)){
+		exit(-1);
 	}
+	int ret = ps->_a[ps->_top - 1];
+	ps->_top--;
+	return ret;
 }
 // 获取栈顶元素 
 STDataType StackTop(Stack* ps){
-	assert(ps);
-	if (!StackEmpty){
-		return ps->_a[ps->_top];
+	if (StackEmpty(ps)){
+		exit(-1);
 	}
-}
-// 获取栈中有效元素个数 
-int StackSize(Stack* ps){
-	assert(ps);
-	return ps->_top;
+	return ps->_a[ps->_top - 1];
 }
 // 检测栈是否为空，如果为空返回非零结果，如果不为空返回0 
 int StackEmpty(Stack* ps){
-	assert(ps);
 	return ps->_top == 0 ? 1 : 0;
 }
 // 销毁栈 
 void StackDestroy(Stack* ps){
-	assert(ps);
 	free(ps->_a);
 	ps->_capacity = 0;
 	ps->_top = 0;
--- 2021-4-17-栈与队列的增删查改/stack.h
+++ 2021-4-17-栈与队列的增删查改/stack.h
@@ -8,15 +8,15 @@ typedef int STDataType;
 typedef struct Stack
 {
 	STDataType* _a;
-	int _top;		// 栈顶
+	int _top;		// 栈顶,指向最后入栈的上面一个位置
 	int _capacity;  // 容量 
 }Stack;
 // 初始化栈 
 void StackInit(Stack* ps);
 // 入栈 
-void StackPush(Stack* ps, STDataType data);
+int StackPush(Stack* ps, STDataType data);
 // 出栈 
-void StackPop(Stack* ps);
+int StackPop(Stack* ps);
 // 获取栈顶元素 
 STDataType StackTop(Stack* ps);
 // 获取栈中有效元素个数 
